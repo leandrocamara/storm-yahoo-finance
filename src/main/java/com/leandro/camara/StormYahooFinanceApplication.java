33909850ab47d2616547621cf0c370273209355b
Leandro Câmara <leandrocamara_eng@outlook.com>
2019-10-13 10:16:46 -0300

    ADD - Create project

---
+++ src/main/java/com/leandro/camara/StormYahooFinanceApplication.java
@@ -0,0 +1,12 @@
+package com.leandro.camara;
+
+/**
+ * Classe responsável pela inicialização do projeto.
+ *
+ * @author Leandro Câmara
+ */
+public class StormYahooFinanceApplication {
+
+    public static void main(String[] args) {
+    }
+}
